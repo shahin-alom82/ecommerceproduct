1081d45c9c73fe2dc64da048f74a90a89c217d30
shahinalom <shahinalom3511371@gmail.com>
2024-10-17 21:08:29 +0600

    add

--- src/compotents/BannerThree.jsx
+++ src/compotents/BannerThree.jsx
@@ -2,20 +2,19 @@ import { app, orangebanner, play } from "../assets";
 
 const BannerThree = () => {
       return (
-            <div className="relative py-10 hidden lg:block">
-                  <img className="lg:w-full h-40" src={orangebanner} alt="orange-banner" />
-                  <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
-                        <h1 className="uppercase lg:text-2xl text-xl text-gray-800">
+            <div className=" py-5  bg-[#678e61] mt-14">
+                  <div className=" inset-0 flex flex-col items-center justify-center text-center">
+                        <h1 className="uppercase lg:text-2xl text-[16px] text-gray-800">
                               Get Notified About New Products
                         </h1>
                         <input
-                              className="border border-gray-300 text-gray-600 py-1 px-4 rounded-full w-[430px] outline-none mt-2"
+                              className="border border-gray-300 text-gray-600 py-1 px-4 rounded-full lg:w-[430px] w-[250px] outline-none mt-2"
                               type="text"
                         />
 
-                        <div className="flex flex-col lg:flex-row mt-4 gap-4">
-                              <img className="h-8 w-28" src={app} alt="app" />
-                              <img className="h-8 w-28" src={play} alt="play" />
+                        <div className="flex mt-4 gap-4">
+                              <img className="lg:h-8 lg:w-28 h-6 w-24" src={app} alt="app" />
+                              <img className="lg:h-8 lg:w-28 h-6 w-24" src={play} alt="play" />
                         </div>
                   </div>
             </div>
